d79ea7da42f010bed3f95ccd9fa94c4c916a3629
VictoriaKush <victoria.v.kush@gmail.com>
2020-02-23 16:03:35 +0100

    ok

---
+++ javascript/javascript1/week1/pizza-exercise/pizza.js
@@ -0,0 +1,5 @@
+console.log("I love pizza");
+let namePizza = 'Hawaiian';
+let pricePizza = 20;
+console.log('New pizza order: ' + namePizza);
+console.log('The price of the pizza is: ' + pricePizza + ' eur');
