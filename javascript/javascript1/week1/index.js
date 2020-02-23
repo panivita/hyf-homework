a1fda4ececf40ff929bf5d74948500541bb2587d
VictoriaKush <victoria.v.kush@gmail.com>
2020-02-23 15:13:30 +0100

    ok

--- javascript/javascript1/week1/index.js
+++ javascript/javascript1/week1/index.js
@@ -1,1 +1,23 @@
-console.log('Hello')
+console.log('Hello world')
+
+var x = 24*55;
+const myName = 'Victoria';
+console.log(x);
+console.log(myName[0]);
+
+const myArr = ['Hello', 'My name is', 'Victoria', 30, 08, 86, true, false, true];
+console.log(myArr[3]);
+console.log(myArr[myArr.length -4]);
+console.log(myName[myName.length -1]);
+
+
+let name = 'benjamin';
+name = 'benjamin-better';
+
+const pizzaPrice = 78;
+const pizzaPriceDiscounted = pizzaPrice - 10;
+
+const users = ['Peter', 'Johnny', 'Børge'];
+
+const lastUser = users[users.length - 1];
+ 
