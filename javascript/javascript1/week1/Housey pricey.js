6778b29f3d9ee4d3d55a3e04301fe0f8f07f676e
VictoriaKush <victoria.v.kush@gmail.com>
2020-02-24 22:30:17 +0100

    change

--- javascript/javascript1/week1/Housey pricey.js
+++ javascript/javascript1/week1/Housey pricey.js
@@ -1,28 +1,31 @@
-let widthOfTheHouse;
-let heightOfTheHouse;
-let depthOfTheHouse;
-let gardenSizeInM2;
-let yourPrice;
+const volumeInMeters = [8*10*10, 5*11*8];
+const gardenSizeInM2 = [100, 70];
+const housePrice = [volumeInMeters[0] * 2.5 * 1000 + gardenSizeInM2[0] * 300, volumeInMeters[1] * 2.5 * 1000 + gardenSizeInM2[1] * 300];
+const name = ['Peter', 'Julia'];
+const conclusionAboutPrice = ['You paying too much,', 'You paying too little,'];
 
 
 
-function volumeInMeters (widthOfTheHouse, heightOfTheHouse, depthOfTheHouse) {
-    return widthOfTheHouse * heightOfTheHouse * depthOfTheHouse;
-}
-
-function housePrice (volumeInMeters, gardenSizeInM2) {
+/*function housePrice (volumeInMeters, gardenSizeInM2) {
     return volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
 }
+*/
 
 
 
 
-
-if (yourPrice > housePrice) {
-    console.log('You paying too much');
+if (housePrice[0] < 2500000) {
+    console.log(conclusionAboutPrice[0] + ' ' + name[0]);
 }
 else {
-    console.log('You paying too little');
+    console.log(conclusionAboutPrice[1] + ' ' + name[0]);
+}
+
+if (housePrice[1] < 1000000 ) {
+    console.log(conclusionAboutPrice[0] + ' ' + name[1]);
+}
+else {
+    console.log(conclusionAboutPrice[1] + ' ' + name[1]);
 }
 
 
