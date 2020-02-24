10dc67d2bb0c536dc529854216bd0bdd75bf946e
VictoriaKush <victoria.v.kush@gmail.com>
2020-02-24 21:39:16 +0100

    working with function

--- javascript/javascript1/week1/Housey pricey.js
+++ javascript/javascript1/week1/Housey pricey.js
@@ -1,8 +1,28 @@
-const widthOfTheHouse;
-const heightOfTheHouse;
-const depthOfTheHouse;
-const gardenSizeInM2;
+let widthOfTheHouse;
+let heightOfTheHouse;
+let depthOfTheHouse;
+let gardenSizeInM2;
+let yourPrice;
 
-let volumeInMeters = widthOfTheHouse * heightOfTheHouse * depthOfTheHouse;
 
-let housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+
+function volumeInMeters (widthOfTheHouse, heightOfTheHouse, depthOfTheHouse) {
+    return widthOfTheHouse * heightOfTheHouse * depthOfTheHouse;
+}
+
+function housePrice (volumeInMeters, gardenSizeInM2) {
+    return volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+}
+
+
+
+
+
+if (yourPrice > housePrice) {
+    console.log('You paying too much');
+}
+else {
+    console.log('You paying too little');
+}
+
+
