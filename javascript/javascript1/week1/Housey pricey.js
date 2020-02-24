ec465c973c1f21588b14c960ae2880c2074a64fb
VictoriaKush <victoria.v.kush@gmail.com>
2020-02-24 23:28:37 +0100

    function again

--- javascript/javascript1/week1/Housey pricey.js
+++ javascript/javascript1/week1/Housey pricey.js
@@ -1,27 +1,41 @@
-const volumeInMeters = [8*10*10, 5*11*8];
-const gardenSizeInM2 = [100, 70];
-const housePrice = [volumeInMeters[0] * 2.5 * 1000 + gardenSizeInM2[0] * 300, volumeInMeters[1] * 2.5 * 1000 + gardenSizeInM2[1] * 300];
 const name = ['Peter', 'Julia'];
 const conclusionAboutPrice = ['You paying too much,', 'You paying too little,'];
-
-
-
-/*function housePrice (volumeInMeters, gardenSizeInM2) {
-    return volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+const petersHouse = {
+    gardenSizeInM2: 100,
+    width: 8,
+    height: 10,
+    depth: 10,
 }
-*/
+
+const juliasHouse = {
+    gardenSizeInM2: 70,
+    width: 5,
+    height: 11,
+    depth: 8,
+}
+
+function houseVolume(house) {
+    return house.width * house.height * house.depth;
+}
+
+function housePrice(house) {
+    const volumeInMeters = houseVolume(house);
+    return volumeInMeters * 2.5 * 1000 + house.gardenSizeInM2 * 300;
+}
+
+const peterHousePrice = housePrice(petersHouse);
+const juliaHousePrice = housePrice(juliasHouse);
 
 
 
-
-if (housePrice[0] < 2500000) {
+if (peterHousePrice < 2500000) {
     console.log(conclusionAboutPrice[0] + ' ' + name[0]);
 }
 else {
     console.log(conclusionAboutPrice[1] + ' ' + name[0]);
 }
 
-if (housePrice[1] < 1000000 ) {
+if (juliaHousePrice < 1000000) {
     console.log(conclusionAboutPrice[0] + ' ' + name[1]);
 }
 else {
