37d9bab8c929262059005124f23396cdda8b582b
VictoriaKush <victoria.v.kush@gmail.com>
2020-02-26 22:44:28 +0100

    implemented feedback

--- javascript/javascript1/week1/pizza-exercise/pizza.js
+++ javascript/javascript1/week1/pizza-exercise/pizza.js
@@ -1,5 +1,5 @@
 console.log("I love pizza");
-let namePizza = 'Hawaiian';
-let pricePizza = 20;
+const namePizza = 'Hawaiian';
+const pricePizza = 20;
 console.log('New pizza order: ' + namePizza);
 console.log('The price of the pizza is: ' + pricePizza + ' eur');
