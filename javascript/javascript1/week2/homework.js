5038a66fc0c988b1231b1f5c693bca988c06fbee
VictoriaKush <victoria.v.kush@gmail.com>
2020-03-02 14:24:41 +0100

    done next exercise

--- javascript/javascript1/week2/homework.js
+++ javascript/javascript1/week2/homework.js
@@ -23,10 +23,30 @@ console.log(fullname);*/
 //Event application
 const howManyDaysFromToday = 2;
 function getEventWeekday() {
-    const date = new Date();
-    const dayName = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday, Saturday'];
-    today = date.getDay();
+    const dayName = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const today = new Date().getDay();
     return dayName[today + howManyDaysFromToday];
 }
 const eventWeekday = getEventWeekday();
 console.log(eventWeekday);
+
+//Weather wear
+function whatWearBasedTemperature(temperature) {
+
+    if (temperature <= 0) {
+        return ' double-layered hooded down jacket';
+    } else if (temperature <= 5) {
+        return ' padded or Puffer Coat, Overcoats';
+    } else if (temperature <= 10) {
+        return' down or padded waterproof jacket';
+    } else if (temperature <= 15) {
+        return ' leather jackets, Biker jackets, Pea Coats';
+    } else if (temperature <= 20) {
+        return ' slip Skirt + Boots + Suede Jacket';
+    } else {
+        return ' shorts and a t-shirt';
+    }
+}
+temperature = 21;
+const clothesToWear = whatWearBasedTemperature(temperature);
+console.log('When the temperature is ' + temperature + ' you should wear' + clothesToWear);
