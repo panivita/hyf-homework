6dbe705ff40d3effda19e0a9e9994076291b7ff5
VictoriaKush <victoria.v.kush@gmail.com>
2020-03-02 13:14:11 +0100

    done exercise 2

--- javascript/javascript1/week2/homework.js
+++ javascript/javascript1/week2/homework.js
@@ -7,7 +7,7 @@ const surname = ['Kush', 'Jensen'];
 const fullname1 = getFullname(firstname[0], surname[0]);
 const fullname2 = getFullname(firstname[1], surname[1]);
 console.log(fullname1);
-console.log(fullname2);*/
+console.log(fullname2);
 
 function getFullname(firstname, lastname, useFormalName) {
     if (useFormalName) {
@@ -18,4 +18,15 @@ function getFullname(firstname, lastname, useFormalName) {
     }
 }
 const fullname = getFullname(firstname, lastname, useFormalName);
-console.log(fullname);
+console.log(fullname);*/
+
+//Event application
+const howManyDaysFromToday = 2;
+function getEventWeekday() {
+    const date = new Date();
+    const dayName = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday, Saturday'];
+    today = date.getDay();
+    return dayName[today + howManyDaysFromToday];
+}
+const eventWeekday = getEventWeekday();
+console.log(eventWeekday);
