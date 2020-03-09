ba665f51cc756fad38cbcae425904fc98cbcbb9b
VictoriaKush <victoria.v.kush@gmail.com>
2020-03-09 14:06:49 +0100

    done exercise My favorite songs

--- javascript/javascript1/week3/Series-duration-of-my-life.js
+++ javascript/javascript1/week3/Series-duration-of-my-life.js
@@ -24,29 +24,26 @@ const seriesDurations = [
         minutes: 30,
     }
 ]
-function seriesWatch(seriesDurations) {
-    let seriesWatchInMinArray = [];
-    for (let i = 0; i < seriesDurations.length; i++) {
-        const daysInMin = seriesDurations[i].days * 1440;
-        const hoursInMin = seriesDurations[i].hours * 60;
-        const min = seriesDurations[i].minutes
-        const seriesWatchInMin = daysInMin + hoursInMin + min;
-        seriesWatchInMinArray.push(seriesWatchInMin);
-    }
-    return seriesWatchInMinArray;
-}
-seriesWatch(seriesDurations);
-console.log(seriesWatch(seriesDurations));
+let totalSum = 0;
 
-const averageLifespanInMin = 80 * 365 * 24 * 60;
-const num = seriesWatch(seriesDurations);
-const result1 = (num[0] * 100) / averageLifespanInMin;
-console.log(result1);
-
-/*function seriesPercentageAverageLifespan(seriesDurations) {
+function seriesPercentageAverageLifespan(seriesWatchInMin) {
     const averageLifespanInMin = 80 * 365 * 24 * 60;
-    const num = seriesWatch(seriesDurations);
-    return (num * 100) / averageLifespanInMin;
-}*/
-//seriesWatchInMinArray = seriesWatchInMinArray[0];
-//const result = seriesPercentageAverageLifespan(seriesDurations);
+    const result = (seriesWatchInMin * 100) / averageLifespanInMin;
+    return result;
+}
+for (let i = 0; i < seriesDurations.length; i++) {
+    const daysInMin = seriesDurations[i].days * 24 * 60;
+    const hoursInMin = seriesDurations[i].hours * 60;
+    const min = seriesDurations[i].minutes
+    const seriesWatchInMin = daysInMin + hoursInMin + min;
+    totalSum += seriesWatchInMin;
+    const calculatePercentage = seriesPercentageAverageLifespan(seriesWatchInMin).toFixed(3);
+    console.log(seriesDurations[i].title + ' took ' + calculatePercentage + '% of my life');
+}
+function totalSeriesDuration () {
+const total = seriesPercentageAverageLifespan(totalSum).toFixed(3);
+    return 'In total that is ' + total + '% of my life';
+}
+
+console.log(totalSeriesDuration());
+
