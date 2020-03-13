fed6eb2f4dfee856951d59b8d6b7f8dd0d108ad1
VictoriaKush <victoria.v.kush@gmail.com>
2020-03-13 18:09:33 +0100

    Implement feedback

--- javascript/javascript1/week3/Item-array-removal.js
+++ javascript/javascript1/week3/Item-array-removal.js
@@ -1,9 +1,11 @@
 const names = ['Peter', 'Ahmad', 'Yana', 'Kristina', 'Rasmus', 'Samuel', 'Katrine', 'Tala'];
 const nameToRemove = 'Ahmad';
 
-for (let i = 0; i < names.length; i++) {
-    if (names[i] === nameToRemove) {
-      names.splice(i, 1);  
-    }
-}      
-console.log(names); 
+function remove(names, nameToRemove) {
+  const i = names.indexOf(nameToRemove);
+  names.splice(i, 1);
+  return names;
+}
+
+const resultRemove = remove(names, nameToRemove); 
+console.log(resultRemove);
