ddf325174231deda8f2cb35b25f91ad2f8e37d95
VictoriaKush <victoria.v.kush@gmail.com>
2020-03-18 20:11:46 +0100

    voice assistant command

--- javascript/javascript1/week4/Voice-assistant.js
+++ javascript/javascript1/week4/Voice-assistant.js
@@ -1,52 +1,122 @@
 function getReply(command) {
-    const sentences = [{
-        question: 'Hello my name is' + name,
-        answer: 'Nice to meet you' + name,
-    },
-    {
-        question: 'What is my name',
-        answer: 'Your name is' + name,
-    },
-    {
-        question: 'Add' + action + 'to my todo',
-        answer: action + 'added to your todo',
-    },
-    {
-        question: 'Remove' + action + 'from my todo',
-        answer: action + 'removed from your todo',
-    },
-    {
-        question: 'What is on my todo?',
-        answer: myTodo,
-    },
-    {
-        question: 'What day is it today?',
-        answer: dayToday,
-    },
-    {
-        question: 'What is' + mathematic ,
-        answer: math,
-    },
-    {
-        question: 'My favorite dish is' + dish,
-        answer: 'Dish saived',
-    },
-    {
-        question: 'What is my favorite dish?',
-        answer: 'Your favorite dish is' + dish,
-    },
-    {
-        question: 'Set a timer for' + minut,
-        answer: 'Timer set for' + minut + 'minutes',
-    },
-    {
-        question: 'Add Bike ride the' + date + 'to my calendar',
-        answer: 'Bike ride added to your calendar',
-    },
-    {
-        question: 'What am I doing this week?',
-        answer: 'This week you have' + eventCalendar.lenght + 'event:' + eventCalendar,
+    const eventCalendar = [];
+    const answers = {
+        answer1: 'Nice to meet you',
+        answer2: 'Your name is',
+        answer3: 'added to your todo',
+        answer4: 'removed from your todo',
+        answer5: 'saived som your favorute dish',
+        answer6: 'Your favorite dish is',
+        answer7: 'Timer set for',
+        answer8: 'Bike ride added to your calendar',
+        answer9: 'This week you have' + eventCalendar.lenght + 'event:' + eventCalendar,
+    };
+    const sentences = {
+        question1: /Hello my name is/,
+        question2: /What is my name/,
+        question3: /Add/,
+        question4: /Remove/,
+        question5: /on my todo?/,
+        question6: /What day is it today?/,
+        question7: /[+ - * /]/,
+        question8: /My favorite dish is/,
+        question9: /What is my favorite dish/,
+        question10: /Set a timer for [0-9] minut/,
+        question11: /Add Bike ride the' + date + 'to my calendar/,
+        question12: /What am I doing this week?/,
+    };
+    function getName() {
+        const n = command.split(" ");
+        const name = n[n.length - 1];
+        function saveName() {
+            if (sentences.question1.test(command)) {
+                return name;
+            }
+        }
+        return saveName();
     }
-    ];
+    const savedName = getName();
+
+    const myTodo = [];
+    function addAction(command) {
+        const n = command.split(" ")
+        const action = n[1];
+        return action;
+    }
+    const action = addAction(command);
+
+    function getDish(command) {
+        const d = command.split(" ");
+        const dish = d.slice(4, d.lenght);
+        return dish.toString();
+    }
+    const dish = getDish(command);
+    
+    function saveDish() {
+        if (sentences.question8.test(command)) {
+            return dish;
+        }
+    }
+    function getMinut(command) {
+        const n = command.split(" ");
+        const minut = n[n.length - 2];
+        return minut; 
+    }
+    const minut = getMinut(command);
+
+    const savedDish = saveDish();
+    if (sentences.question1.test(command)) {
+        return `${answers.answer1} ${savedName}`;
+    }
+    if (sentences.question2.test(command)) {
+        return `${answers.answer2} ${savedName}`;
+    }
+    if (sentences.question3.test(command)) {
+        //myTodo.push(savedAction);
+        return `${action} ${answers.answer3}`;
+    }
+    if (sentences.question4.test(command)) {
+        //myTodo.pop(savedAction);
+        return `${action} ${answers.answer4}`;
+    }
+    if (sentences.question5.test(command)) {
+        //myTodo.push(savedAction);
+        return myTodo;
+    }
+    if (sentences.question6.test(command)) {
+        const d = new Date();
+        const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
+        const mo = new Intl.DateTimeFormat('en', { month: 'long' }).format(d);
+        const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
+        const dayToday = `${da}. of ${mo} ${ye}`;
+        return dayToday;
+    }
+    /*if (sentences.question7.test(command)) {
+        const exemple = command.slice(8, command.lenght);
+        const math = eval(exemple);
+        return `${exemple} = ${math}`;
+    }*/
+    if (sentences.question8.test(command)) {
+        return `${dish} ${answers.answer5}`;
+    }
+    if (sentences.question9.test(command)) {
+        return `${answers.answer6} ${savedDish}`;
+    }
+    if (sentences.question10.test(command)) {
+        return `${answers.answer7} ${minut} minutes`;
+    }
 
 }
+
+console.log(getReply('Hello my name is Benjamin'));
+console.log(getReply('What is my name?'));
+console.log(getReply('Add fishing to my todo'));
+console.log(getReply('Remove fishing from my todo'));
+console.log(getReply('Add singing in the shower to my todo'));
+//console.log(getReply('What is on my todo?'));
+console.log(getReply('What day is it today?'));
+console.log(getReply('What is 3 + 3'));
+console.log(getReply('My favorite dish is lasagne'));
+//console.log(getReply('What is my favorite dish'));
+console.log(getReply('Set a timer for 4 minutes'));
+//console.log(getReply());
