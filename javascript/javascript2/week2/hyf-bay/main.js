93bd0430858f17e0f1102fd48bf1edc3d25c8e78
VictoriaKush <victoria.v.kush@gmail.com>
2020-04-01 00:44:00 +0200

    done input search products

---
+++ javascript/javascript2/week2/hyf-bay/main.js
@@ -0,0 +1,48 @@
+console.log('Script loaded');
+
+function makerLists(className, key) {
+    const lists = document.createElement('li');
+    lists.classList.add(className);
+    lists.innerHTML = key;
+    return lists;
+}
+
+function renderProducts(products) {
+    const ul = document.querySelector('section.products > ul');
+    ul.innerHTML = null;
+    products.forEach((object) => {
+        const li = document.createElement('li');
+        ul.appendChild(li);
+        const subUl = document.createElement('ul');
+        li.appendChild(subUl);
+        subUl.appendChild(makerLists('id', object.id));
+        subUl.appendChild(makerLists('name', object.name));
+        subUl.appendChild(makerLists('price', object.price));
+        subUl.appendChild(makerLists('rating', object.rating));
+
+        const subLi = document.createElement('li');
+        subLi.classList.add('ships-to');
+        const shipsToUl = document.createElement('ul');
+        subLi.appendChild(shipsToUl);
+        subUl.appendChild(subLi);
+
+        object.shipsTo.forEach((country) => {
+            const countryLi = document.createElement('li');
+            countryLi.innerHTML = country;
+            shipsToUl.appendChild(countryLi);
+        })
+    });
+}
+const products = getAvailableProducts();
+renderProducts(products);
+
+const inputSearch = document.querySelector('.search > input');
+inputSearch.addEventListener('keyup', () => {
+    if (inputSearch.value) {
+        const filteredProducts = products.filter((p) => RegExp(inputSearch.value, 'i').test(p.name));
+        renderProducts(filteredProducts);
+    } else {
+        renderProducts(products);
+    }
+})
+
