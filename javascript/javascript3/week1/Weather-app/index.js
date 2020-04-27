1cc63b30fa881901087c5d05aa19bcdb3c1b467a
VictoriaKush <victoria.v.kush@gmail.com>
2020-04-27 23:39:59 +0200

    done map

--- javascript/javascript3/week1/Weather-app/index.js
+++ javascript/javascript3/week1/Weather-app/index.js
@@ -1,3 +1,6 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoidmljdG9yaWEta3VzaCIsImEiOiJjazlpd3kwYm4wMnRvM25wOHQzYzZ0dWU2In0.vPB-ZEW7LHcbhuaEISB3Sg";
+
 const getCity = (tag) => {
   return tag.value.replace(/^\s+|\s+$/g, "");
 };
@@ -7,7 +10,9 @@ const convertTimestamptoTime = (unixT) => {
   const hours = dateObj.getUTCHours();
   const minutes = dateObj.getUTCMinutes();
 
-  return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
+  return `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}`;
 };
 
 const inputTag = document.getElementById("input");
@@ -15,6 +20,40 @@ inputTag.focus();
 const btnTag = document.getElementById("get-weather");
 const pTag = document.getElementById("error");
 const ulTag = document.getElementById("weather-data");
+
+const renderResult = (result) => {
+  const mainData = result.main;
+  const temp = (mainData.temp - 273.15).toFixed(1);
+  const windData = result.wind;
+  const cloudsData = result.clouds;
+  const weatherData = result.weather[0];
+  const sunData = result.sys;
+
+  const output = `
+    <li>${result.name}</li>
+    <li>${temp}°C</li>
+    <li><i class="owf owf-${weatherData.id} owf-5x"></i>${
+    weatherData.description
+  }</li>
+    <li>Wind ${windData.speed}m/s.</li>
+    <li>Clouds ${cloudsData.all}%</li> 
+    <li>Sunrise: ${convertTimestamptoTime(sunData.sunrise)}</li>
+    <li>Sunset: ${convertTimestamptoTime(sunData.sunset)}</li>
+    `;
+  ulTag.innerHTML = output;
+  return result;
+};
+
+const renderMap = (result) => {
+  const { lon, lat } = result.coord;
+  const map = new mapboxgl.Map({
+    container: "map",
+    center: [lon, lat],
+    zoom: 9,
+    style: "mapbox://styles/mapbox/streets-v11",
+  });
+  return result;
+};
 
 const onClickHandler = () => {
   const city = getCity(inputTag);
@@ -25,27 +64,8 @@ const onClickHandler = () => {
     //const url = `https://api.openweathermap.org/data/2.5/weather?zip=${city},DK&appid=4555a73df1a819f049cc845a8d9338bf`;
     fetch(url)
       .then((res) => res.json())
-      .then((result) => {
-        const mainData = result.main;
-        const temp = (mainData.temp - 273.15).toFixed(1);
-        const windData = result.wind;
-        const cloudsData = result.clouds;
-        const weatherData = result.weather[0];
-        const sunData = result.sys;
-console.log(sunData);
-        const output = `
-        <li>${result.name}</li>
-        <li>${temp}°C</li>
-        <li><i class="owf owf-${weatherData.id} owf-5x"></i>${
-          weatherData.description
-        }</li>
-        <li>Wind ${windData.speed}m/s.</li>
-        <li>Clouds ${cloudsData.all}%</li> 
-        <li>Sunrise: ${convertTimestamptoTime(sunData.sunrise)}</li>
-        <li>Sunset: ${convertTimestamptoTime(sunData.sunset)}</li>
-        `;
-        ulTag.innerHTML = output;
-      });
+      .then(renderResult)
+      .then(renderMap);
   }
 };
 btnTag.addEventListener("click", onClickHandler);
