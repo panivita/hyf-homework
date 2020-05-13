1b6a18652afe8b8a33e9cd87346500cd9f30e3a4
VictoriaKush <victoria.v.kush@gmail.com>
2020-05-13 23:13:15 +0200

    button remove

--- javascript/javascript3/week3/Shopping-cart/index.js
+++ javascript/javascript3/week3/Shopping-cart/index.js
@@ -28,10 +28,9 @@ class ShoppingCart {
   }
   //removeProduct should remove a product from the products array.
   removeProduct(product) {
-    const newProduct = this.products.filter((existing) => {
+    this.products = this.products.filter((existing) => {
       return existing.name !== product.name;
     });
-    this.products = newProduct;
   }
   //searchProduct should return an array of product that match the productName parameter
   searchProduct(productName) {
@@ -45,11 +44,13 @@ class ShoppingCart {
   // Also render the username and the total price of the products in the shoppingcart
   renderProducts() {
     const shoppingCart = document.getElementById("shopping-cart");
+    shoppingCart.innerHTML = null;
     const ulProducts = document.createElement("ul");
     shoppingCart.appendChild(ulProducts);
     this.products.forEach((product) => {
       ulProducts.innerHTML += `<li><img src="${product.imgUrl}"></img>
-      <span>${product.name}</span> <span>${product.price} EUR</span></li>`;
+      <span>${product.name}</span> <span>${product.price} EUR</span>
+      <button>Remove</button></li>`;
     });
     const total = document.createElement("p");
     shoppingCart.appendChild(total);
@@ -84,52 +85,48 @@ class ProductList {
   }
   renderProductList() {
     const productsSection = this.container;
+    productsSection.innerHTML = null;
     this.products.forEach((product) => {
-      const liProduct = document.createElement("li");
-      liProduct.innerHTML = `
-        <ul>
-            <li><img src="${product.imgUrl}"></img></li>
-            <li>${product.name}</li>
-            <li>${product.price}EUR</li>
-        </ul>`;
-      productsSection.appendChild(liProduct);
-      const buttonLi = document.createElement("li");
+      const productCont = document.createElement("div");
+      productsSection.appendChild(productCont);
+      productCont.innerHTML = `
+            <div><img src="${product.imgUrl}"></img></div>
+            <div>${product.name}</div>
+            <div>${product.price}EUR</div>`;
+      const buttonDiv = document.createElement("div");
       const btn = document.createElement("Button");
       btn.innerHTML = "Add to cart";
-      buttonLi.appendChild(btn);
-      liProduct.appendChild(buttonLi);
-      //btn.addEventListener("click", () => shoppingCart.addProduct(product));
+      buttonDiv.appendChild(btn);
+      productCont.appendChild(buttonDiv);
+
+      btn.addEventListener("click", () => {
+        shoppingCart.removeProduct(product);
+        shoppingCart.addProduct(product);
+        shoppingCart.renderProducts();
+      });
     });
   }
+  
 }
 
 const shoppingCart = new ShoppingCart();
-// shoppingCart.addProduct(dress1);
-// shoppingCart.addProduct(dress7);
-// shoppingCart.addProduct(dress8);
-// shoppingCart.removeProduct(dress9);
-// console.log(shoppingCart.getTotal());
-// shoppingCart.getUser();
-// shoppingCart.renderProducts();
-// console.log(shoppingCart);
 
 //Searching for products
 const inputSearch = document.querySelector(".search > input");
-
 inputSearch.addEventListener("keyup", () => {
   const productName = inputSearch.value;
-  const divSearch = document.getElementById("search-results");
+  const divSearch = document.querySelector(".search-results");
   divSearch.innerHTML = "";
   if (productName) {
     const ulSearch = document.createElement("ul");
     divSearch.appendChild(ulSearch);
     shoppingCart.searchProduct(productName).forEach((product) => {
-      ulSearch.innerHTML = `<li><span>${product.name}</span>: <span>${product.price} EUR</span></li>`;
+      ulSearch.innerHTML = `<li><img src="${product.imgUrl}"></img><span>${product.name}</span> <span>${product.price} EUR</span></li>`;
     });
   }
 });
 
-new ProductList(document.querySelector("section.products ul"));
+new ProductList(document.querySelector("section.products"));
 
 // Assuming dkr as default currency
 const plant = new Product("plant", 50);
