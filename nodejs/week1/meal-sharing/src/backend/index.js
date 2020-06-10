d8bb5e6ac571e6c416a341683c8095d91f6c44a7
VictoriaKush <victoria.v.kush@gmail.com>
2020-06-10 21:21:21 +0200

    implemented feedback

--- nodejs/week1/meal-sharing/src/backend/index.js
+++ nodejs/week1/meal-sharing/src/backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Instead of writing the functionality for the routes inside index.js. 
+// Instead of writing the functionality for the routes inside index.js.
 // Create a routes folder that contains meals.js, meal.js, etc.
 const mealsRouter = require("./routes/meals");
 const cheapMealsRouter = require("./routes/cheap-meals");
@@ -20,15 +20,15 @@ app.get("/reservation", reservationRouter);
 
 app.get("/", (req, res) => {
   res.send(`
-<ul>
+  <ul>
     <li><a href='/meals'>all-meals</a></li>
     <li><a href='/cheap-meals'>cheap-meals</a></li>
     <li><a href='/large-meals'>large-meals</a></li>
     <li><a href='/meal'>single-meal</a></li>
     <li><a href='/reservations'>all-reservations</a></li>
     <li><a href='/reservation'>single-reservation</a></li>
-</ul>   
-`);
+  </ul>
+  `);
 });
 
 app.listen(port, () =>
