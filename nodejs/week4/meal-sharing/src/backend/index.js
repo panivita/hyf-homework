e170aa6860494e46bcb1e75600dd590d97ee5732
VictoriaKush <victoria.v.kush@gmail.com>
2020-07-05 01:17:49 +0200

    heroku

--- nodejs/week4/meal-sharing/src/backend/index.js
+++ nodejs/week4/meal-sharing/src/backend/index.js
@@ -1,13 +1,15 @@
+console.log("index.js 1");
+
 const express = require("express");
 const app = express();
 const router = express.Router();
 const path = require("path");
 const port = process.env.PORT || 3000;
-
+console.log("index.js 2");
 const mealsRouter = require("./api/meals");
 const reservationsRouter = require("./api/reservations");
 const reviewsRouter = require("./api/reviews");
-
+console.log("index.js 3");
 // For week4 no need to look into this!
 // Serve the built client html
 const buildPath = path.join(__dirname, "./../frontend");
@@ -25,7 +27,7 @@ router.use("/reviews", reviewsRouter);
 app.use("/api", router);
 
 app.get("/", (req, res) => {
-  res.send('karrrramba');
+  res.send("karrrramba");
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
