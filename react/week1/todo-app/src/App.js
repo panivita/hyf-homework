93ebf0b2ca0c6d2b78abedd1e9ae346e45418bc0
LAPTOP-SDLVUQ69\Iryna <victoria.v.kush@gmail.com>
2020-08-06 00:55:03 +0200

    done timer

--- react/week1/todo-app/src/App.js
+++ react/week1/todo-app/src/App.js
@@ -1,37 +1,54 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 const TodoList = (props) => {
-  const todoItems = props.data.map((todo, index) =>
-  <li key={index}>
-    {todo.description}, {todo.deadline}
-  </li>
-);
-  return (
-    <ul>{todoItems}</ul>
-  );
-}
+  const todoItems = props.data.map((todo) => (
+    <li key={todo.id}>{todo.description}</li>
+  ));
+  return <ul>{todoItems}</ul>;
+};
 
 const TodoListSection = (props) => {
   return (
     <section className="todoList">
-      <TodoList data={props.data}/>
+      <TodoList data={props.data} />
     </section>
   );
-}
+};
+
+const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
+  useEffect(() => {
+    const id = setInterval(() => {
+      setSeconds((s) => s + 1);
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+  return <p>You have used {seconds} seconds on this website</p>;
+};
 
 const App = () => {
   const todoData = [
-    {description: "Get out of bed" , deadline: "Wed Sep 13 2017"},
-    {description: "Brush teeth" , deadline: "Thu Sep 14 2017"},
-    {description: "Eat breakfast" , deadline: "Fri Sep 15 2017"},
+    {
+      id: 1,
+      description: "Get out of bed",
+    },
+    {
+      id: 2,
+      description: "Brush teeth",
+    },
+    {
+      id: 3,
+      description: "Eat breakfast",
+    },
   ];
   return (
     <>
       <h1>My ToDo list</h1>
+      <Timer />
       <TodoListSection data={todoData} />
     </>
   );
-}
+};
 
 export default App;
