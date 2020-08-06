a9c0f19067a3cf05b1f94224fe3d9cda50684070
LAPTOP-SDLVUQ69\Iryna <victoria.v.kush@gmail.com>
2020-08-07 00:48:27 +0200

    done handlecheck

--- react/week1/todo-app/src/App.js
+++ react/week1/todo-app/src/App.js
@@ -1,9 +1,25 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-const TodoList = ({ data = [] }) => {
+const TodoList = ({ data = [], handleCheck }) => {
+  const deleteItem = (id) => {
+    const [stateDelete, setDelete] = useState(id);
+    setDelete(stateDelete.filter((t) => t.id !== id));
+  };
   const todoItems = data.map((todo) => (
-    <li key={todo.id}>{todo.description}</li>
+    <li key={todo.id}>
+      <input
+        type="checkbox"
+        name={todo.description}
+        checked={todo.done && "checked"}
+        onChange={() => handleCheck(todo.id)}
+      />
+      <span className={todo.done ? "line-through" : "no-decoration"}>
+        {todo.description}
+      </span>
+
+      <button onClick={() => deleteItem(todo.id)}>Delete</button>
+    </li>
   ));
   return <ul>{todoItems}</ul>;
 };
@@ -12,13 +28,28 @@ const TodoListSection = ({ list = [] }) => {
   const [todos, setTodos] = useState(list);
   const addTodo = () => {
     setTodos((t) => {
-      const item = { id: 4, description: "Random text" };
+      const item = {
+        id: t.length + 1,
+        description: "Random text",
+        done: false,
+      };
       return [...t, item];
+    });
+  };
+  const handleCheck = (id) => {
+    setTodos((stateCheck) => {
+      const check = stateCheck.map((t) => {
+        if (t.id === id) {
+          return { ...t, done: !t.done };
+        }
+        return t;
+      });
+      return check
     });
   };
   return (
     <section className="todoList">
-      <TodoList data={todos} />
+      <TodoList data={todos} handleCheck={handleCheck} />
       <button className="add-todo" onClick={addTodo}>
         Add todo
       </button>
@@ -42,14 +73,17 @@ const App = () => {
     {
       id: 1,
       description: "Get out of bed",
+      done: false,
     },
     {
       id: 2,
       description: "Brush teeth",
+      done: false,
     },
     {
       id: 3,
       description: "Eat breakfast",
+      done: false,
     },
   ];
   return (
