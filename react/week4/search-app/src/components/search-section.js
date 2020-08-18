f3c5266abb26e127b4db2d3d3700caa3ed762abc
VictoriaKush <victoria.v.kush@gmail.com>
2020-08-18 18:43:52 +0200

    add styling

--- react/week4/search-app/src/components/search-section.js
+++ react/week4/search-app/src/components/search-section.js
@@ -4,20 +4,19 @@ export const Search = () => {
   const [search, setSearch] = useState("");
   const [display, setDisplay] = useState(false);
   const [options, setOptions] = useState([]);
-  const [err, setError] = useState("");
   const wrapperRef = useRef(null);
 
   useEffect(() => {
     fetch(`https://api.github.com/search/users?q=${search}`)
       .then((res) => res.json())
       .then((data) => setOptions(data))
-      .catch((err) => setError(err.message));
+      .catch((err) => console.log(err));
   }, [search]);
 
   useEffect(() => {
-    document.addEventListener("mousedown", handleClickerOutside);
+    document.addEventListener("keyup", handleClickerOutside);
     return () => {
-      document.removeEventListener("mousedown", handleClickerOutside);
+      document.removeEventListener("keyup", handleClickerOutside);
     };
   }, []);
 
@@ -27,6 +26,12 @@ export const Search = () => {
       setDisplay(false);
     }
   };
+
+  const setSearchUser = (user) => {
+    setSearch(user);
+    setDisplay(false);
+  };
+
   return (
     <div ref={wrapperRef} className="search-container">
       <input
@@ -36,15 +41,20 @@ export const Search = () => {
             setDisplay(!display);
           }
         }}
-        placeholder="Search github user"
+        placeholder="Search for user"
         value={search}
         onChange={(event) => setSearch(event.target.value)}
       />
       {display && (
         <ul className="result-container">
-          {options.map(({ id, login }) => (
-            <li className="options" key={id} tabIndex="0">
-              <span>{login}</span>
+          {options.items.map(({ id, login }) => (
+            <li
+              className="options"
+              key={id}
+              tabIndex="0"
+              onClick={() => setSearchUser({ login })}
+            >
+              {login}
             </li>
           ))}
         </ul>
