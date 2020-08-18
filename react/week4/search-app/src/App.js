6d55a0d467d95a2f0985d78e33ea9ab1993e50ee
VictoriaKush <victoria.v.kush@gmail.com>
2020-08-18 11:56:21 +0200

    start working this homework

---
+++ react/week4/search-app/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import { Header } from "./components/header";
+import { Search } from "./components/search-section";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Search />
+    </>
+  );
+};
+
+export default App;
+
+
